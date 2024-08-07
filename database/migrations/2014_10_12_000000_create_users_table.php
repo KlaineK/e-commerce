5dd328c2485c9a0be0b52ecbbe5197e7fb58e9d7
KlaineK <keshiadarma@gmail.com>
2024-08-07 16:57:55 +0700

    Day 1 : Migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->longText('address');
+            $table->enum('role', ['user', 'admin']);
+            $table->enum('status', ['active', 'warning','blacklist']);
             $table->rememberToken();
             $table->timestamps();
         });
